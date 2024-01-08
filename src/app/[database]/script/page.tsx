3c7b012c68a1df761ad213a07e3414f86c99d6ce
Lorenzo <https.braganca@gmail.com>
2024-01-08 08:36:24 -0300

    feat(wip): scripts page

--- src/app/[database]/script/page.tsx
+++ src/app/[database]/script/page.tsx
@@ -1,8 +1,11 @@
-import { ChevronRight, Redo2, Undo2, X } from "lucide-react";
+import { Redo2, Undo2 } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
-import { TextArea } from "@/components/ui/textarea";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+
+import { ClearButton } from "./_components/buttons/clear";
+import { SubmitButton } from "./_components/buttons/submit";
+import { ScriptTextArea } from "./_components/script-text-area";
 
 export default function Page() {
    return (
@@ -10,9 +13,7 @@ export default function Page() {
          <div data-toolbar className="flex w-full items-center gap-2">
             <Tooltip>
                <TooltipTrigger asChild>
-                  <Button intent="outline" size="icon-sm">
-                     <X className="h-4 w-4 shrink-0" height={16} width={16} />
-                  </Button>
+                  <ClearButton />
                </TooltipTrigger>
                <TooltipContent>Clears all the content</TooltipContent>
             </Tooltip>
@@ -34,21 +35,12 @@ export default function Page() {
             </Tooltip>
             <Tooltip>
                <TooltipTrigger asChild>
-                  <Button intent="outline" size="icon-sm" className="group/button ml-auto">
-                     <div className="relative h-4 w-4 shrink-0">
-                        <ChevronRight
-                           className="absolute inset-y-0 left-0 h-4 w-4 shrink-0 duration-150 group-hover/button:left-[4px]"
-                           height={16}
-                           width={16}
-                        />
-                        <div className="absolute right-[8px] top-1/2 h-px w-0 -translate-x-full -translate-y-1/2 bg-zinc-700 duration-150 group-hover/button:-right-[7px] group-hover/button:w-2.5 dark:bg-zinc-50" />
-                     </div>
-                  </Button>
+                  <SubmitButton />
                </TooltipTrigger>
                <TooltipContent>Run your script</TooltipContent>
             </Tooltip>
          </div>
-         <TextArea className="h-full w-full grow" intent="primary" />
+         <ScriptTextArea />
       </main>
    );
 }
