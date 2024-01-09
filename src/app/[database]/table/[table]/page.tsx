e9fa6115885d5c37694b15d56adb71a5ab18579a
Lorenzo <https.braganca@gmail.com>
2024-01-09 09:41:09 -0300

    style: improved ui

--- src/app/[database]/table/[table]/page.tsx
+++ src/app/[database]/table/[table]/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { cookies } from "next/headers";
-import { Check, X } from "lucide-react";
+import { Check, ChevronDown, MoreVertical, X } from "lucide-react";
 import * as pg from "pg";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
 const getTable = async (uuid: string, table: string) => {
    let client;
@@ -15,7 +17,7 @@ const getTable = async (uuid: string, table: string) => {
       client = new pg.Client({ connectionString: database?.[uuid]?.url });
 
       await client.connect();
-      const res = await client.query(`SELECT * FROM ${table}`);
+      const res = await client.query(`SELECT * FROM ${table} ORDER BY 1`);
 
       return { ...res };
    } catch (err) {
@@ -34,10 +36,17 @@ export default async function Page({ params }: { params: { database: string; tab
       <div className="modern-scroll max-h-[calc(100dvh-37px)] w-full overflow-x-auto">
          <table className="h-fit w-full overflow-auto text-sm">
             <thead className="sticky left-0 top-0 h-9 border-b border-b-zinc-200 bg-zinc-100 text-zinc-700 dark:border-b-zinc-800 dark:bg-zinc-900 dark:text-zinc-300">
-               <tr className="px-2">
+               <tr>
                   {table?.fields?.map((field: any) => (
-                     <th key={field.columnID} className="px-3 font-normal">
-                        {field.name}
+                     <th
+                        key={field.columnID}
+                        className="border-r border-r-zinc-200 px-3 font-normal last:border-r-0 dark:border-r-zinc-800">
+                        <div className="flex items-center justify-between gap-2">
+                           <span>{field.name}</span>
+                           <Button size="icon-custom" intent="ghost" className="-mr-2 size-7">
+                              <ChevronDown className="size-4 shrink-0" />
+                           </Button>
+                        </div>
                      </th>
                   ))}
                </tr>
@@ -45,11 +54,15 @@ export default async function Page({ params }: { params: { database: string; tab
             <tbody className="text-zinc-800 dark:text-zinc-200">
                {table?.rows?.map((row: any, index: number) => (
                   <tr
-                     className="h-10 border-b border-b-zinc-200 hover:bg-zinc-100 hover:text-zinc-950 dark:border-b-zinc-800 dark:hover:bg-zinc-900 dark:hover:text-zinc-50"
+                     className="h-7 border-b border-b-zinc-200 hover:bg-zinc-100 hover:text-zinc-950 dark:border-b-zinc-800 dark:hover:bg-zinc-900 dark:hover:text-zinc-50"
                      key={index}>
                      {Object?.values?.(row)?.map((cell: any) => (
                         <td
-                           className="truncate border-r border-r-zinc-200 px-2 last:border-r-0 dark:border-r-zinc-800"
+                           className={cn(
+                              "truncate border-r border-r-zinc-200 px-2 last:border-r-0 dark:border-r-zinc-800",
+                              typeof cell == "number" && "text-right",
+                              typeof cell == "object" && cell?.constructor?.name === "Date" && "text-right",
+                           )}
                            key={cell}>
                            {typeof cell === "boolean" &&
                               (cell ? (
