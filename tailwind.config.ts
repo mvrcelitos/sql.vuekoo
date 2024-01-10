0da692a9daa3b34fa58e31c5963c648e235aaf85
Lorenzo <https.braganca@gmail.com>
2024-01-10 10:28:50 -0300

    feat: added some accordion animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,6 +21,20 @@ const config: Config = {
             background: "var(--background)",
             foreground: "var(--foreground)",
          },
+         keyframes: {
+            "accordion-down": {
+               from: { height: "0" },
+               to: { height: "var(--radix-accordion-content-height)" },
+            },
+            "accordion-up": {
+               from: { height: "var(--radix-accordion-content-height)" },
+               to: { height: "0" },
+            },
+         },
+         animation: {
+            "accordion-down": "accordion-down 0.2s ease-out",
+            "accordion-up": "accordion-up 0.2s ease-out",
+         },
       },
    },
    plugins: [require("tailwindcss-animate")],
