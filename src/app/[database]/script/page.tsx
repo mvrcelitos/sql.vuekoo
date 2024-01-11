21bce1ea3d7078ae796d534e20aed610235a77df
Lorenzo <https.braganca@gmail.com>
2024-01-11 15:17:36 -0300

    refactor: implementing `flexdiv`

--- src/app/[database]/script/page.tsx
+++ src/app/[database]/script/page.tsx
@@ -6,41 +6,48 @@ import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip
 
 import { ClearButton, SubmitButton } from "./_components/buttons";
 import { ScriptTextArea } from "./_components/script-textarea";
+import { FlexDiv } from "@/components/ui/layouts";
 
 export default function Page() {
    return (
-      <main className="flex h-full max-h-[calc(100dvh-37px)] grow flex-col gap-2 overflow-hidden p-6 md:gap-4">
-         <div data-toolbar className="flex w-full items-center gap-2">
-            <Tooltip>
-               <TooltipTrigger asChild>
-                  <ClearButton />
-               </TooltipTrigger>
-               <TooltipContent>Clears all the content</TooltipContent>
-            </Tooltip>
-            <Tooltip>
-               <TooltipTrigger asChild>
-                  <Button disabled intent="outline" size="icon-sm">
-                     <Undo2 className="h-4 w-4 shrink-0" height={16} width={16} />
-                  </Button>
-               </TooltipTrigger>
-               <TooltipContent>Undo the last action</TooltipContent>
-            </Tooltip>
-            <Tooltip>
-               <TooltipTrigger asChild>
-                  <Button disabled intent="outline" size="icon-sm">
-                     <Redo2 className="h-4 w-4 shrink-0" height={16} width={16} />
-                  </Button>
-               </TooltipTrigger>
-               <TooltipContent>Redo your last action</TooltipContent>
-            </Tooltip>
-            <Tooltip>
-               <TooltipTrigger asChild>
-                  <SubmitButton />
-               </TooltipTrigger>
-               <TooltipContent>Run your script</TooltipContent>
-            </Tooltip>
-         </div>
-         <ScriptTextArea />
+      <main className="flex h-full grow flex-col gap-2 overflow-hidden p-6 md:gap-4">
+         <FlexDiv className="gap-2 md:gap-4">
+            <FlexDiv orientation="vertical" className="shrink-0 gap-2">
+               <Tooltip>
+                  <TooltipTrigger asChild>
+                     <SubmitButton />
+                  </TooltipTrigger>
+                  <TooltipContent>Run your script</TooltipContent>
+               </Tooltip>
+
+               <Tooltip>
+                  <TooltipTrigger asChild>
+                     <Button disabled intent="outline" size="icon-sm">
+                        <Redo2 className="h-4 w-4 shrink-0" height={16} width={16} />
+                     </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>Redo your last action</TooltipContent>
+               </Tooltip>
+
+               <Tooltip>
+                  <TooltipTrigger asChild>
+                     <Button disabled intent="outline" size="icon-sm">
+                        <Undo2 className="h-4 w-4 shrink-0" height={16} width={16} />
+                     </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>Undo the last action</TooltipContent>
+               </Tooltip>
+
+               <Tooltip>
+                  <TooltipTrigger asChild>
+                     <ClearButton />
+                  </TooltipTrigger>
+                  <TooltipContent>Clears all the content</TooltipContent>
+               </Tooltip>
+            </FlexDiv>
+            <ScriptTextArea />
+         </FlexDiv>
+
          <ScriptResult />
       </main>
    );
