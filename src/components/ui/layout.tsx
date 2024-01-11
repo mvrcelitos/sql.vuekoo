b7e34938d62abbad892408aa4327301cc9bb32a3
Lorenzo <https.braganca@gmail.com>
2024-01-11 15:41:56 -0300

    feat: implemented grid div and removed unused `center`

--- src/components/ui/layout.tsx
+++ src/components/ui/layout.tsx
@@ -3,21 +3,13 @@ import { cva, VariantProps } from "cva";
 
 import { cn } from "@/lib/utils";
 
-export const Center = React.forwardRef<HTMLDivElement, React.ComponentPropsWithoutRef<"div">>(
-   ({ className, ...props }, ref) => {
-      return (
-         <div ref={ref} className={cn("flex items-center justify-center overflow-hidden p-1", className)} {...props} />
-      );
-   },
-);
-Center.displayName = "Center";
-
-export const flexVariants = cva({
+export const flexDivVariants = cva({
    base: "flex overflow-hidden",
    variants: {
       orientation: {
          horizontal: "flex-row w-full",
          vertical: "flex-col h-full",
+         center: "items-center justify-center",
       },
    },
    defaultVariants: {
@@ -25,11 +17,43 @@ export const flexVariants = cva({
    },
 });
 
-export interface FlexProps extends React.ComponentPropsWithoutRef<"div">, VariantProps<typeof flexVariants> {}
+export interface FlexDivProps extends React.ComponentPropsWithoutRef<"div">, VariantProps<typeof flexDivVariants> {}
 
-export const FlexDiv = React.forwardRef<HTMLDivElement, FlexProps>(({ className, orientation, ...props }, ref) => {
+export const FlexDiv = React.forwardRef<HTMLDivElement, FlexDivProps>(({ className, orientation, ...props }, ref) => {
    return (
-      <div data-orientation={orientation} ref={ref} className={flexVariants({ orientation, className })} {...props} />
+      <div
+         data-orientation={orientation}
+         ref={ref}
+         className={flexDivVariants({ orientation, className })}
+         {...props}
+      />
    );
 });
 FlexDiv.displayName = "FlexDiv";
+
+export const gridDivVariants = cva({
+   base: "grid overflow-hidden",
+   variants: {
+      orientation: {
+         horizontal: "grid-rows-1 auto-cols-auto w-full",
+         vertical: "grid-cols-1 auto-rows-auto h-full",
+      },
+   },
+   defaultVariants: {
+      orientation: "horizontal",
+   },
+});
+
+export interface GridDivProps extends React.ComponentPropsWithoutRef<"div">, VariantProps<typeof flexDivVariants> {}
+
+export const GridDiv = React.forwardRef<HTMLDivElement, FlexDivProps>(({ className, orientation, ...props }, ref) => {
+   return (
+      <div
+         data-orientation={orientation}
+         ref={ref}
+         className={flexDivVariants({ orientation, className })}
+         {...props}
+      />
+   );
+});
+GridDiv.displayName = "GridDiv";
