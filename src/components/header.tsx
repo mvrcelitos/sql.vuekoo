8ce155cefc0c51149d62db8e22a7377845bfe7b9
Lorenzo <https.braganca@gmail.com>
2024-01-15 11:01:49 -0300

    feat: new header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,5 @@
-import * as React from "react";
-import Link from "next/link";
-
 import { HeaderThemeContent } from "@/components/header-theme-content";
-import { Button } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import {
    DropdownMenu,
    DropdownMenuContent,
@@ -13,85 +10,56 @@ import {
    DropdownMenuSubTrigger,
    DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { cn } from "@/lib/utils";
+import { Menu } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { ScriptButton } from "@/components/header-buttons";
 
 export const Header = () => {
    return (
-      <header className="sticky left-0 top-0 z-40 w-full shrink-0 border-b border-b-zinc-200 bg-background px-4 py-1.5 text-foreground dark:border-b-zinc-800">
-         <div data-tauri-drag-region className="flex items-center justify-between">
-            <div className="flex items-center gap-1">
-               <div className="select-none text-sm font-bold uppercase">
-                  <span className="text-zinc-700 dark:text-zinc-300">VUEKOO</span>
-                  <span className="text-base text-primary">/</span>
-                  <span className="text-zinc-900 dark:text-zinc-50">SQL</span>
-               </div>
-               <hr className="ml-2 mr-1 h-4 w-px grow border-none bg-zinc-200 dark:bg-zinc-800" />
-               <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                     <Button
-                        intent="ghost"
-                        size="alt"
-                        className="text-[13px] data-[state=open]:bg-zinc-200 data-[state=open]:text-zinc-800 dark:data-[state=open]:bg-zinc-800/70 dark:data-[state=open]:text-zinc-200">
-                        File
-                     </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent align="start" sideOffset={2}>
-                     <DropdownMenuItem className="text-[13px]">New connection</DropdownMenuItem>
-                     <DropdownMenuItem className="text-[13px]">Share</DropdownMenuItem>
-                     <DropdownMenuSeparator />
+      <header className="sticky left-0 top-0 z-40 flex h-[37px] w-full shrink-0 border-b border-b-zinc-200 bg-background text-foreground dark:border-b-zinc-800">
+         {/* <div data-tauri-drag-region={undefined} className="flex items-center justify-between"> */}
+         {/* <div className="flex grow items-center gap-1"> */}
+         <DropdownMenu>
+            <DropdownMenuTrigger
+               className={cn(
+                  buttonVariants({ intent: "ghost", size: "icon-sm" }),
+                  "rounded-none text-[13px] data-[state=open]:bg-zinc-200 data-[state=open]:text-zinc-800 dark:data-[state=open]:bg-zinc-800/70 dark:data-[state=open]:text-zinc-200",
+               )}>
+               <Menu className="size-4 shrink-0" height={16} width={16} />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="start" sideOffset={2}>
+               <DropdownMenuItem className="text-[13px]">New connection</DropdownMenuItem>
+               <DropdownMenuItem disabled className="text-[13px]">
+                  Share
+               </DropdownMenuItem>
+               <DropdownMenuSeparator />
+               <DropdownMenuSub>
+                  <DropdownMenuSubTrigger className="text-[13px]">Preferences</DropdownMenuSubTrigger>
+                  <DropdownMenuSubContent>
                      <DropdownMenuSub>
-                        <DropdownMenuSubTrigger className="text-[13px]">Preferences</DropdownMenuSubTrigger>
-                        <DropdownMenuSubContent>
-                           <DropdownMenuSub>
-                              <DropdownMenuSubTrigger className="text-[13px]">Theme</DropdownMenuSubTrigger>
-                              <HeaderThemeContent />
-                           </DropdownMenuSub>
-                        </DropdownMenuSubContent>
+                        <DropdownMenuSubTrigger className="text-[13px]">Theme</DropdownMenuSubTrigger>
+                        <HeaderThemeContent />
                      </DropdownMenuSub>
-                  </DropdownMenuContent>
-               </DropdownMenu>
-               <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                     <Button
-                        intent="ghost"
-                        size="alt"
-                        className="text-[13px] data-[state=open]:bg-zinc-200 data-[state=open]:text-zinc-800 dark:data-[state=open]:bg-zinc-800/70 dark:data-[state=open]:text-zinc-200">
-                        Edit
-                     </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent align="start" sideOffset={2}>
-                     <DropdownMenuItem className="text-[13px]">Undo</DropdownMenuItem>
-                     <DropdownMenuItem disabled>Redo</DropdownMenuItem>
-                     <DropdownMenuSeparator />
-                     <DropdownMenuItem className="text-[13px]">Cut</DropdownMenuItem>
-                     <DropdownMenuItem className="text-[13px]">Copy</DropdownMenuItem>
-                     <DropdownMenuItem className="text-[13px]">Paste</DropdownMenuItem>
-                  </DropdownMenuContent>
-               </DropdownMenu>
-               <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                     <Button
-                        intent="ghost"
-                        size="alt"
-                        className="text-[13px] data-[state=open]:bg-zinc-200 data-[state=open]:text-zinc-800 dark:data-[state=open]:bg-zinc-800/70 dark:data-[state=open]:text-zinc-200">
-                        Help
-                     </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent align="start" sideOffset={2}>
-                     <DropdownMenuItem className="text-[13px]" asChild>
-                        <Link href="https://github.com/mvrcelitos/sql.vuekoo">Source code</Link>
-                     </DropdownMenuItem>
-                     <DropdownMenuItem className="text-[13px]" asChild>
-                        <Link href="/welcome">Welcome</Link>
-                     </DropdownMenuItem>
-                  </DropdownMenuContent>
-               </DropdownMenu>
-            </div>
-            {/* <div className="-my-1.5 -mr-4 flex h-9 justify-center gap-1">
+                  </DropdownMenuSubContent>
+               </DropdownMenuSub>
+            </DropdownMenuContent>
+         </DropdownMenu>
+         {/* <Separator orientation="vertical" className="h-4 w-px bg-zinc-300 dark:bg-zinc-700" /> */}
+         <div className="select-none self-center px-2 text-sm font-bold uppercase">
+            <span className="text-zinc-700 dark:text-zinc-300">VUEKOO</span>
+            <span className="text-base text-primary">/</span>
+            <span className="text-zinc-900 dark:text-zinc-50">SQL</span>
+         </div>
+         <Separator orientation="vertical" className="mx-1 h-5 w-px self-center bg-zinc-200 dark:bg-zinc-800" />
+         <ScriptButton />
+         {/* </div> */}
+         {/* <div className="-my-1.5 -mr-4 flex h-9 justify-center gap-1">
                <MinimizeButton />
                <MaximizeButton />
                <CloseButton />
             </div> */}
-            {/* <div className="flex items-center gap-4">
+         {/* <div className="flex items-center gap-4">
                <DropdownMenu>
                   <DropdownMenuTrigger className="-my-0.5 flex h-8 w-8 items-center justify-center rounded-full bg-zinc-800 text-zinc-300 dark:bg-zinc-100 dark:text-zinc-600">
                      <UserRound className="h-4 w-4 shrink-0" height={16} width={16} />
@@ -117,7 +85,7 @@ export const Header = () => {
                   </DropdownMenuContent>
                </DropdownMenu>
             </div> */}
-         </div>
+         {/* </div> */}
       </header>
    );
 };
