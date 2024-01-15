42f43089c46f0679c7fe3c719c92249b0b105af5
Lorenzo <https.braganca@gmail.com>
2024-01-15 11:15:21 -0300

    fix: implemented new data table component

--- src/app/[database]/table/[table]/page.tsx
+++ src/app/[database]/table/[table]/page.tsx
@@ -8,6 +8,8 @@ import { Flex } from "@/components/ui/layout";
 import { Table, TableWrapper, TBody, Td, Th, THead, TRow } from "@/components/ui/table";
 
 import { Toolbar } from "./_components/toolbar";
+import { DataTable } from "@/components/data-table";
+import { DataTableToolbar } from "@/components/data-table-toolbar";
 
 // export const revalidate = 60;
 
@@ -58,65 +60,10 @@ export default async function Page({ params, searchParams }: { params: paramsPro
    return (
       <Flex child="main" orientation="vertical">
          <TableWrapper>
-            <Table>
-               <THead className="sticky left-0 top-0 h-9 border-b border-b-zinc-200 bg-zinc-100 text-zinc-700 dark:border-b-zinc-800 dark:bg-zinc-900 dark:text-zinc-300">
-                  <TRow>
-                     <Th className="sticky left-0 z-10 w-0 border-zinc-300 bg-zinc-200 dark:border-zinc-700 dark:bg-zinc-800">
-                        #
-                     </Th>
-                     {table?.fields?.map((field: any) => (
-                        <TableColumnHeader key={field.columnID} id={field.name}>
-                           {field.name}
-                        </TableColumnHeader>
-                     ))}
-                  </TRow>
-               </THead>
-               <TBody>
-                  {table?.rows?.map((row: any, index: number) => (
-                     <TRow
-                        className="h-7 border-b border-b-zinc-200 hover:bg-zinc-100 hover:text-zinc-950 dark:border-b-zinc-800 dark:hover:bg-zinc-900 dark:hover:text-zinc-50"
-                        key={index}>
-                        <Td className="sticky left-0 z-10 w-0 bg-zinc-100 dark:bg-zinc-900">{index + 1}</Td>
-                        {table?.fields?.map((field: any, index: number) => {
-                           const cell = row[field.name];
-                           switch (cell?.constructor?.name) {
-                              case "Date":
-                                 return (
-                                    <Td key={field.columnID} className="text-right">
-                                       {(cell as Date).toLocaleString()}
-                                    </Td>
-                                 );
-                              case "Number":
-                                 return (
-                                    <Td key={field.columnID} className="text-right">
-                                       {(cell as Date).toLocaleString()}
-                                    </Td>
-                                 );
-                              case "String":
-                                 return <Td key={field.columnID}>{cell}</Td>;
-                              case "Boolean":
-                                 const Icon = cell ? Check : X;
-                                 return (
-                                    <Td key={field.columnID}>
-                                       <Icon className="mx-auto size-5 shrink-0" size={20} />
-                                    </Td>
-                                 );
-                              default:
-                                 if (cell === null)
-                                    return (
-                                       <Td key={field.columnID} className="text-center opacity-50">
-                                          {"[NULL]"}
-                                       </Td>
-                                    );
-                                 return <Td key={field.columnID}>{cell}</Td>;
-                           }
-                        })}
-                     </TRow>
-                  ))}
-               </TBody>
-            </Table>
+            <DataTable fields={table?.fields as any} rows={table?.rows} />
          </TableWrapper>
-         <Toolbar rows={table?.rowCount ?? undefined} />
+         <DataTableToolbar rows={table?.rowCount} />
+         {/* <Toolbar rows={table?.rowCount ?? undefined} /> */}
       </Flex>
    );
 }
