d6dbc96a47ca7e99092372f8750f3a3a7a1e2264
Lorenzo <https.braganca@gmail.com>
2024-01-19 22:57:11 -0300

    fix: added border top

--- src/app/[database]/[table]/data/loading.tsx
+++ src/app/[database]/[table]/data/loading.tsx
@@ -2,7 +2,7 @@ import { Loader2 } from "lucide-react";
 
 export default function Loading() {
    return (
-      <main className="flex h-full flex-auto flex-col items-center justify-center overflow-hidden">
+      <main className="flex h-full flex-auto flex-col items-center justify-center overflow-hidden border-t border-t-zinc-200 dark:border-t-zinc-800">
          <Loader2 className="shirnk-0 h-5 w-5 animate-spin" />
       </main>
    );
