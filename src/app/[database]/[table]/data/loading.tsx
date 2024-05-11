9536398f45aed74af1c2e537650990269b1bd2ca
Lorenzo <https.braganca@gmail.com>
2024-05-11 18:07:01 -0300

    fix: removed unused classes

--- src/app/[database]/[table]/data/loading.tsx
+++ src/app/[database]/[table]/data/loading.tsx
@@ -1,9 +1,13 @@
+import { DataTableToolbar } from "@/components/data-table-toolbar";
 import { Loader2 } from "lucide-react";
 
 export default function Loading() {
    return (
-      <main className="flex h-full flex-auto flex-col items-center justify-center overflow-hidden border-t border-t-zinc-200 dark:border-t-zinc-800">
-         <Loader2 className="shirnk-0 h-5 w-5 animate-spin" />
+      <main className="flex flex-auto flex-col overflow-hidden border-t border-t-zinc-200 dark:border-t-zinc-800">
+         <div className="flex shrink-0 grow items-center justify-center overflow-hidden">
+            <Loader2 className="shirnk-0 h-5 w-5 animate-spin" />
+         </div>
+         <DataTableToolbar rows={0} />
       </main>
    );
 }
