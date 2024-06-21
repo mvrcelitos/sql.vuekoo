22e8e43cb3a0dbd9fe63c39a5d5740a1ae7e02e9
Lorenzo <https.braganca@gmail.com>
2024-06-21 00:15:21 -0300

    fix: removed unused imports

--- src/components/ui/radio.tsx
+++ src/components/ui/radio.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useEffect, useRef, useState } from "react";
+import { forwardRef, useState } from "react";
 
 import { cn } from "@/lib/utils";
 import { AnimatePresence, motion } from "framer-motion";
