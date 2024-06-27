dbc6a6e75a31b94b68a8b2fbd97c3d601ea6142e
Lorenzo <https.braganca@gmail.com>
2024-06-27 16:37:27 -0300

    fix: adjusted class name that was typed wrong

--- src/app/()/databases/[uuid]/[table]/()/data/loading.tsx
+++ src/app/()/databases/[uuid]/[table]/()/data/loading.tsx
@@ -6,7 +6,7 @@ export default function Loading() {
    return (
       <main className="flex flex-auto flex-col overflow-hidden border-t border-t-zinc-200 dark:border-t-zinc-800">
          <div className="flex shrink-0 grow items-center justify-center overflow-hidden">
-            <Loader2 className="shirnk-0 h-5 w-5 animate-spin" />
+            <Loader2 className="size-5 shrink-0 animate-spin" />
          </div>
          <DataTableToolbar rows={0} />
       </main>
