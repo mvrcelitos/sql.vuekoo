7358c4ac7308edba34880e53b2c767c1b8ce5f4f
Lorenzo <https.braganca@gmail.com>
2024-06-27 16:08:06 -0300

    feat: added some animations on page loading

--- src/app/()/page.tsx
+++ src/app/()/page.tsx
@@ -1,5 +1,6 @@
 import { cookies } from "next/headers";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
@@ -22,30 +23,37 @@ export default function Page() {
       return (
          <main className="flex h-full w-full flex-initial flex-col items-center justify-center overflow-hidden p-4 text-foreground">
             <div className="flex max-w-md flex-col gap-2">
-               <h1 className="text-center text-2xl font-bold md:text-3xl">👋 Hello</h1>
-               <p className="text-center text-sm opacity-80 md:text-base">
-                  If you are lost, sql/vuekoo is a web application too help you to see and manage your databases,
-                  tables, and more.
+               <h1 className="animate-content-in text-center text-2xl font-bold duration-500 [--stagger:0] md:text-3xl">
+                  👋 Hello
+               </h1>
+               <p className="animate-content-in text-center text-sm opacity-80 duration-500 [--stagger:1] md:text-base">
+                  If you are lost, sql/vuekoo is a web application to help you to see and manage your databases, tables,
+                  and more.
                </p>
-               <div className="flex items-center gap-2 sm:my-2">
+               <div className="animate-content-in flex items-center gap-2 duration-500 [--stagger:2] sm:my-2">
                   <span className={cn("block h-0.5 w-12 bg-zinc-200  dark:bg-zinc-800")} />
                   <span className={cn("block h-0.5 w-full grow bg-zinc-200 dark:bg-zinc-800")} />
                   <span className={cn("block h-0.5 w-12 bg-zinc-200  dark:bg-zinc-800")} />
                </div>
                <div className="mb-2 flex flex-col gap-2 sm:mb-4 sm:gap-4">
-                  <p className="text-center text-sm opacity-80 md:text-base">
+                  <p className="animate-content-in text-center text-sm opacity-80 duration-500 [--stagger:3] md:text-base">
                      You can create an account to save your databases, settings and then you access them from anywhere.
                   </p>
-                  <p className="text-center text-sm opacity-80 md:text-base">
+                  <p className="animate-content-in text-center text-sm opacity-80 duration-500 [--stagger:4] md:text-base">
                      But this isn{"'"}t necessary, you can continue as guest if you want, but your data will be lost
                      when you close the browser.
                   </p>
                </div>
                <div className="flex items-center justify-center gap-0.5 md:gap-4 md:text-base">
-                  <Link href={"#"} className={cn(buttonVariants({ intent: "ghost", size: "lg" }), "rounded-full")}>
+                  <Link
+                     href={"#"}
+                     className={cn(
+                        buttonVariants({ intent: "ghost", size: "lg" }),
+                        "animate-content-in rounded-full [--stagger:5]",
+                     )}>
                      I need help
                   </Link>
-                  <GetStartedButton />
+                  <GetStartedButton className="animate-content-in [--stagger:6]" />
                </div>
                {/* <p className="mb-2 text-left text-sm opacity-70 last-of-type:mb-0 sm:text-base md:mb-4">
                   We are currently suggesting our users to create an account on our system.
