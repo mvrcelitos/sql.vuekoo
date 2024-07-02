db6ed04f476b18d569c6ddb956276bd6bcfaa81a
Lorenzo <https.braganca@gmail.com>
2024-07-02 16:44:35 -0300

    feat: dialog body

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -21,7 +21,7 @@ const DialogOverlay = React.forwardRef<
    <DialogPrimitive.Overlay
       ref={ref}
       className={cn(
-         "fixed inset-0 z-50 bg-zinc-950/60 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+         "fixed inset-0 z-[101] bg-black/60 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 dark:bg-black/40",
          className,
       )}
       {...props}
@@ -38,12 +38,12 @@ const DialogContent = React.forwardRef<
       <DialogPrimitive.Content
          ref={ref}
          className={cn(
-            "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border border-muted bg-accent p-6 shadow-sm duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-xl",
+            "fixed left-[50%] top-[50%] z-[101] grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 rounded-xl border border-muted bg-background duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
             className,
          )}
          {...props}>
          {children}
-         <DialogPrimitive.Close className="absolute right-4 top-4 rounded-md bg-transparent p-0.5 text-zinc-500 opacity-70 transition-opacity hover:opacity-100 focus:bg-zinc-200 focus:text-zinc-700 focus:outline-none disabled:pointer-events-none dark:bg-transparent dark:text-zinc-400 dark:focus:bg-zinc-800 dark:focus:text-zinc-200">
+         <DialogPrimitive.Close className="absolute right-3 top-3 rounded-md bg-transparent p-1.5 text-zinc-500 opacity-70 !outline-none transition-opacity hover:opacity-100 disabled:pointer-events-none hocus:bg-zinc-200 hocus:text-zinc-700 dark:bg-transparent dark:text-zinc-400 dark:focus:bg-zinc-800 dark:focus:text-zinc-200">
             <X className="h-4 w-4" />
             <span className="sr-only">Close</span>
          </DialogPrimitive.Close>
@@ -53,12 +53,25 @@ const DialogContent = React.forwardRef<
 DialogContent.displayName = DialogPrimitive.Content.displayName;
 
 const DialogHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
-   <div className={cn("flex flex-col text-center sm:text-left", className)} {...props} />
+   <div
+      className={cn("flex flex-col border-b border-muted px-4 py-5 text-center sm:text-left", className)}
+      {...props}
+   />
 );
 DialogHeader.displayName = "DialogHeader";
 
+const DialogBody = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+   <div className={cn("bg-background px-4 py-2", className)} {...props} />
+);
+
 const DialogFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
-   <div className={cn("flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2", className)} {...props} />
+   <div
+      className={cn(
+         "flex items-center justify-between gap-2 rounded-b-xl border-t border-muted bg-accent px-4 py-3 [&>button]:!highlight-0",
+         className,
+      )}
+      {...props}
+   />
 );
 DialogFooter.displayName = "DialogFooter";
 
@@ -69,7 +82,7 @@ const DialogTitle = React.forwardRef<
    <DialogPrimitive.Title
       ref={ref}
       {...props}
-      className={cn("text-xl font-semibold leading-none tracking-tight text-foreground", className)}
+      className={cn("text-base font-medium leading-none tracking-tight text-foreground", className)}
    />
 ));
 DialogTitle.displayName = DialogPrimitive.Title.displayName;
@@ -87,6 +100,7 @@ export {
    DialogClose,
    DialogContent,
    DialogDescription,
+   DialogBody,
    DialogFooter,
    DialogHeader,
    DialogOverlay,
