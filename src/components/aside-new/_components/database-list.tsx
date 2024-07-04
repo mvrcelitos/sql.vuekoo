d0c038f7940458994c4fba7a3b3ddb29ddc97edc
Lorenzo <https.braganca@gmail.com>
2024-07-03 23:43:19 -0300

    fix: removed unused animate presence

--- src/components/aside-new/_components/database-list.tsx
+++ src/components/aside-new/_components/database-list.tsx
@@ -12,23 +12,18 @@ export const DatabaseList = ({ databases }: { databases: DatabasesReturn }) => {
 
    return (
       <div className="flex flex-col gap-0.5" onMouseLeave={() => setHover(null)}>
-         <AnimatePresence>
-            {databases?.map((database, index) => {
-               return (
-                  <DatabaseItem
-                     key={database.uuid}
-                     // initial={{ opacity: 0, y: 10 }}
-                     // animate={{ opacity: 1, y: 0 }}
-                     // exit={{ opacity: 0, y: 10 }}
-                     database={database}
-                     index={index}
-                     count={databases.length || 0}
-                     hover={hover == index}
-                     onMouseEnter={() => setHover(index)}
-                  />
-               );
-            })}
-         </AnimatePresence>
+         {databases?.map((database, index) => {
+            return (
+               <DatabaseItem
+                  key={database.uuid}
+                  database={database}
+                  index={index}
+                  count={databases.length || 0}
+                  hover={hover === index}
+                  onMouseEnter={() => setHover(index)}
+               />
+            );
+         })}
       </div>
    );
 };
