0b4a23a0a01a190237c70447606c4798aa4bee4d
Lorenzo <https.braganca@gmail.com>
2024-07-03 23:07:30 -0300

    feat: adjusted themes of sonner

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -14,9 +14,9 @@ const Toaster = ({ ...props }: ToasterProps) => {
          className="toaster group"
          toastOptions={{
             classNames: {
-               toast: "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border",
-               error: "group-[.toaster]:bg-red-500 dark:group-[.toaster]:bg-red-500 group-[.toaster]:text-zinc-50",
-               description: "group-[.toast]:text-muted-foreground",
+               toast: "group toast group-[.toaster]:bg-primary dark:group-[.toaster]:bg-foreground group-[.toaster]:text-background group-[.toaster]:border-transparent",
+               error: "group-[.toaster]:!bg-red-500 dark:group-[.toaster]:!bg-red-600 group-[.toaster]:!text-zinc-50",
+               description: "group-[.toast]:text-muted",
                actionButton: "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
                cancelButton: "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
             },
