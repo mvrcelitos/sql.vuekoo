7079c2479f0cc4f63b47450cc42323019d783c63
Lorenzo <https.braganca@gmail.com>
2024-07-19 12:54:32 -0300

    fix: adjusted height

--- src/app/()/layout.tsx
+++ src/app/()/layout.tsx
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
    return (
       <>
-         <div className="flex min-h-[calc(100sdvh-var(--header-height))] flex-initial flex-col md:flex-row">
+         <div className="flex min-h-[calc(100dvh-var(--header-height))] flex-initial flex-col md:flex-row">
             <NavigationMenu />
             <AsideNew />
             {/* <Aside /> */}
