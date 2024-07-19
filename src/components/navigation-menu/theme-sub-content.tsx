678234782c706dd2fc2c840a815d5a49798a33d0
Lorenzo <https.braganca@gmail.com>
2024-07-19 09:07:14 -0300

    feat: changed label of dropdown menu items and adjusted check icon to right

--- src/components/navigation-menu/theme-sub-content.tsx
+++ src/components/navigation-menu/theme-sub-content.tsx
@@ -11,18 +11,18 @@ export const ThemeSubContent = () => {
       <DropdownMenuSubContent>
          <DropdownMenuItem intent="default" className="text-[13px]" onClick={() => setTheme("light")}>
             <Sun className="mr-2 size-4 shrink-0" />
-            <span>Light theme</span>
-            {theme == "light" && <Check className="ml-4 size-4 shrink-0" height={16} width={16} />}
+            <span>Light</span>
+            {theme == "light" && <Check className="ml-auto size-4 shrink-0 opacity-70" height={16} width={16} />}
          </DropdownMenuItem>
          <DropdownMenuItem intent="default" className="text-[13px]" onClick={() => setTheme("dark")}>
             <Moon className="mr-2 size-4 shrink-0" />
-            <span>Dark theme</span>
-            {theme == "dark" && <Check className="ml-4 size-4 shrink-0" height={16} width={16} />}
+            <span>Dark</span>
+            {theme == "dark" && <Check className="ml-auto size-4 shrink-0 opacity-70" height={16} width={16} />}
          </DropdownMenuItem>
          <DropdownMenuItem intent="default" className="text-[13px]" onClick={() => setTheme("system")}>
             <Laptop className="mr-2 size-4 shrink-0" />
             <span>System</span>
-            {theme == "system" && <Check className="ml-4 size-4 shrink-0" height={16} width={16} />}
+            {theme == "system" && <Check className="ml-auto size-4 shrink-0 opacity-70" height={16} width={16} />}
          </DropdownMenuItem>
       </DropdownMenuSubContent>
    );
