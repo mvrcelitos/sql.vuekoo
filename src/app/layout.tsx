8cd07b747605f4a79cd75f3488c275a7a8181c98
Lorenzo <https.braganca@gmail.com>
2024-08-14 10:58:04 -0300

    feat: add script provider on root app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,14 @@ import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
 import { GeistSans } from "geist/font/sans";
 
-import Header from "@/components/header";
+import { Header } from "@/components/header";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { config } from "@/config/site";
 import { cn } from "@/lib/utils";
 
 import "./globals.css";
+import { ScriptProvider } from "@/components/script-dialog";
 
 export const metadata: Metadata = {
    title: {
@@ -28,9 +29,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <html lang="en">
          <body className={cn("flex h-dvh max-h-lvh min-h-svh flex-col overflow-hidden", GeistSans.className)}>
             <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-               <Header />
-               {children}
-               <Toaster />
+               <ScriptProvider>
+                  <Header />
+                  {children}
+                  <Toaster />
+               </ScriptProvider>
             </ThemeProvider>
             <Analytics />
          </body>
