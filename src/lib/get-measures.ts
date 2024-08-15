17694fb4564f8d339f8cfdddd95e9961cbbeb14c
Lorenzo <https.braganca@gmail.com>
2024-08-15 09:50:19 -0300

    feat: add use callback on handle resize

--- src/lib/get-measures.ts
+++ src/lib/get-measures.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
 export const getWidth = () => {
    const [width, setWidth] = useState<number>(0);
@@ -43,32 +43,36 @@ const breakpoints = {
    "2xl": 1536,
 } as const;
 
-export const getBreakpoints = () => {
-   const [value, setValue] = useState<keyof typeof breakpoints | null>(null);
+// export const getBreakpoints = () => {
+//    const [value, setValue] = useState<keyof typeof breakpoints | null>(null);
 
-   useEffect(() => {
-      if (typeof window === "undefined") return;
-      // setValue(window.innerWidth >= breakpoints[breakpoint]);
-      const handleResize = () => {
-         const entries = Object.entries(breakpoints);
-         const result = entries.reduce((acc, cur) => {
-            if (cur[1] < window.innerWidth) return acc;
-            return cur[1] < acc[1] ? cur : acc;
-         }, entries[0]);
-         setValue(result[0] as keyof typeof breakpoints);
-      };
-      window.addEventListener("resize", handleResize);
-      return () => window.removeEventListener("resize", handleResize);
-   }, []);
-};
+//    const handleResize = useCallback(() => {
+//       const entries = Object.entries(breakpoints);
+//       const result = entries.reduce((acc, cur) => {
+//          if (cur[1] < window.innerWidth) return acc;
+//          return cur[1] < acc[1] ? cur : acc;
+//       }, entries[0]);
+//       setValue(result[0] as keyof typeof breakpoints);
+//    }, []);
+
+//    useEffect(() => {
+//       if (typeof window === "undefined") return;
+//       // setValue(window.innerWidth >= breakpoints[breakpoint]);
+//       window.addEventListener("resize", handleResize);
+//       return () => window.removeEventListener("resize", handleResize);
+//    }, []);
+// };
 
 export const getBreakpoint = (breakpoint: keyof typeof breakpoints) => {
    const [value, setValue] = useState<boolean>(false);
    const breakpointWidth = breakpoints[breakpoint];
 
+   const handleResize = useCallback(() => {
+      setValue(window.innerWidth >= breakpointWidth);
+   }, []);
+
    useEffect(() => {
       if (typeof window === "undefined") return;
-      const handleResize = () => setValue(window.innerWidth >= breakpointWidth);
       window.addEventListener("resize", handleResize);
       handleResize();
       return () => window.removeEventListener("resize", handleResize);
