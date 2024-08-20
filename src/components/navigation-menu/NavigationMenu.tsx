20f9409df8c4b854c10f6779531847403938bb9f
mvrcelo <https.braganca@gmail.com>
2024-08-20 14:42:36 -0300

    feat: change to new system of navigation menu

--- src/components/navigation-menu/NavigationMenu.tsx
+++ src/components/navigation-menu/NavigationMenu.tsx
@@ -1,25 +1,13 @@
 "use client";
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { AnimatePresence, motion } from "framer-motion";
-import { Code, Palette, Settings } from "lucide-react";
 
-import { NavigationMenuItem, navigationMenuItems } from "@/components/navigation-menu/data";
-import { ThemeSubContent } from "@/components/navigation-menu/theme-sub-content";
-import { Button } from "@/components/ui/button";
-import {
-   DropdownMenu,
-   DropdownMenuContent,
-   DropdownMenuItem,
-   DropdownMenuSub,
-   DropdownMenuSubTrigger,
-   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
-import { getBreakpoint } from "@/lib/get-measures";
 import { cn } from "@/lib/utils";
-import { useNavigationMenuStore } from "@/components/navigation-menu/NavigationMenu.Store";
+import { NavigationMenuItem, navigationMenuItems } from "@/components/navigation-menu/data";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+
+import { useNavigationMenuStore } from "./NavigationMenu.Store";
+import { useState } from "react";
 
 export const NavigationMenu = () => {
    const { selected, setSelected } = useNavigationMenuStore();
@@ -28,30 +16,31 @@ export const NavigationMenu = () => {
       <nav className="z-[2] flex h-[--nav-size] w-full flex-row items-center justify-between gap-2 border-muted bg-accent [--nav-size:45px] max-md:border-b sm:gap-4 md:h-full md:w-[--nav-size] md:max-w-[--nav-size] md:flex-col md:border-r">
          <div className="flex flex-row items-center gap-1 p-1 md:flex-col md:gap-4">
             {navigationMenuItems.map((props, index) => {
+               const { disabled, name, slug } = props;
                const isActive = selected === props.slug;
+
                return (
                   <div key={index} className="flex items-center">
                      <Item
-                        {...props}
+                        name={name}
+                        aria-label={slug}
                         aria-selected={isActive}
-                        onClick={() => setSelected(isActive ? null : props.slug)}>
+                        aria-disabled={disabled}
+                        onClick={() => setSelected(selected === props.slug ? null : props.slug)}>
                         <AnimatePresence>
-                           {isActive && (
+                           {isActive ? (
                               <motion.div
+                                 key={slug}
                                  layoutId="navigation-menu-item-active"
-                                 initial={{ opacity: 0, scale: 0, rotate: -45 }}
-                                 animate={{
-                                    opacity: 1,
-                                    scale: 1,
-                                    rotate: 0,
-                                 }}
-                                 exit={{ opacity: 0, rotate: 45 }}
+                                 initial={{ opacity: 0 }}
+                                 animate={{ opacity: 1 }}
+                                 exit={{ opacity: 0 }}
                                  transition={{ type: "spring", duration: 0.3, bounce: 0 }}
                                  className={cn(
                                     "pointer-events-none absolute inset-0 rounded-md bg-primary/15 shadow-[0_1px_1px_rgba(0,0,0,.02)] dark:highlight-5",
                                  )}
                               />
-                           )}
+                           ) : null}
                         </AnimatePresence>
                         <props.icon
                            className={cn(
@@ -70,17 +59,17 @@ export const NavigationMenu = () => {
    );
 };
 
-const Item = ({
-   name,
-   icon: Icon,
-   ...props
-}: Pick<NavigationMenuItem, "name" | "icon"> & React.ComponentPropsWithoutRef<typeof Button>) => {
+const Item = ({ name, ...props }: Pick<NavigationMenuItem, "name"> & React.ComponentPropsWithoutRef<"button">) => {
+   const [tooltip, setTooltip] = useState<boolean>(false);
+
    return (
       <Tooltip>
          <TooltipTrigger asChild>
-            <Button {...props} intent="none" className="group relative size-9 rounded-md p-2">
+            <button
+               {...props}
+               className="group relative size-9 rounded-md p-2 !outline-none aria-disabled:pointer-events-none aria-disabled:opacity-50">
                {props?.children}
-            </Button>
+            </button>
          </TooltipTrigger>
          <TooltipContent side="right" sideOffset={4}>
             {name}
