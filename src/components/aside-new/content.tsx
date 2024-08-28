a7022316d5d97fe33da670927f9971fe5ad97685
mvrcelo <https.braganca@gmail.com>
2024-08-27 21:40:27 -0300

    fix: improve animation

--- src/components/aside-new/content.tsx
+++ src/components/aside-new/content.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 import { DatabasesList } from "@/components/aside-new/_components/database-list";
 import { useNavigationMenuStore } from "@/components/navigation-menu/store";
@@ -8,65 +8,61 @@ import { DatabaseType } from "@/interfaces/cookies/databases";
 import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { NavigationSlugs, navigationSlugs } from "@/components/navigation-menu/data";
 
 export const AsideContent = ({ databases }: { databases: DatabaseType[] }) => {
+   const previousDirection = useRef<number>(0);
+   const previousSelected = useRef<NavigationSlugs>(null);
    const { selected } = useNavigationMenuStore();
-   const [direction, setDirection] = useState<number>(0);
 
-   return useMemo(() => {
-      let content: React.ReactNode;
-      switch (selected) {
-         case "databases":
-            content = (
-               <motion.div
-                  key="databases"
-                  className="flex flex-col"
-                  initial={{
-                     translateY: "-110%",
-                     opacity: 0,
-                  }}
-                  animate={{
-                     translateY: 0,
-                     opacity: 1,
-                  }}
-                  exit={{
-                     translateY: "-110%",
-                     opacity: 0,
-                  }}
-                  transition={{ type: "spring", duration: 0.4, bounce: 0.1 }}>
-                  <DatabasesList databases={databases ?? []} />
-               </motion.div>
-            );
-            break;
-         case "terminals":
-            content = (
-               <motion.div
-                  key="sql-terminal"
-                  className="flex flex-col"
-                  initial={{
-                     translateY: "110%",
-                     opacity: 0,
-                  }}
-                  animate={{
-                     translateY: 0,
-                     opacity: 1,
-                  }}
-                  exit={{
-                     translateY: "110%",
-                     opacity: 0,
-                  }}
-                  transition={{ type: "spring", duration: 0.4, bounce: 0.1 }}>
-                  <AsideSQLs databases={databases} />
-               </motion.div>
-            );
-            break;
-      }
-      return (
-         <AnimatePresence initial={false} mode="popLayout">
-            {content}
-         </AnimatePresence>
-      );
-   }, [selected, databases]);
+   // previousSelected.current = selected;
+   const direction =
+      previousSelected.current === null || selected === null
+         ? 0
+         : Math.sign(navigationSlugs.indexOf(selected) - navigationSlugs.indexOf(previousSelected.current));
+   console.log(JSON.stringify({ selected, previousSelected: previousSelected.current, direction }));
+   // @ts-ignore
+   previousDirection.current = previousSelected.current === selected ? previousDirection.current : direction;
+   // @ts-ignore
+   previousSelected.current = selected;
+   let content: React.ReactNode;
+   switch (selected) {
+      case "databases":
+         content = (
+            <motion.div
+               key="databases"
+               className="flex flex-col"
+               variants={variants}
+               initial="initial"
+               animate="animate"
+               exit="exit"
+               custom={previousDirection.current}
+               transition={{ type: "spring", duration: 0.4, bounce: 0.1 }}>
+               <DatabasesList databases={databases ?? []} />
+            </motion.div>
+         );
+         break;
+      case "terminals":
+         content = (
+            <motion.div
+               key="sql-terminal"
+               className="flex flex-col"
+               variants={variants}
+               initial="initial"
+               animate="animate"
+               exit="exit"
+               custom={previousDirection.current}
+               transition={{ type: "spring", duration: 0.4, bounce: 0.1 }}>
+               <AsideSQLs databases={databases} />
+            </motion.div>
+         );
+         break;
+   }
+   return (
+      <AnimatePresence initial={false} mode="popLayout" custom={previousDirection.current}>
+         {content}
+      </AnimatePresence>
+   );
 };
 
 const AsideSQLs = ({ databases }: { databases: DatabaseType[] }) => {
@@ -78,6 +74,7 @@ const AsideSQLs = ({ databases }: { databases: DatabaseType[] }) => {
             const url = `/terminals/${database.uuid}`;
             return (
                <Link
+                  key={url}
                   href={url}
                   className="group relative flex h-[calc(2.25rem+1px)] items-center justify-start border-b border-b-muted px-1 text-sm text-zinc-800 hover:bg-accent dark:text-zinc-200">
                   {/* <motion.div
@@ -100,4 +97,10 @@ const AsideSQLs = ({ databases }: { databases: DatabaseType[] }) => {
          })}
       </div>
    );
+};
+
+const variants = {
+   initial: (direction: number) => ({ opacity: 0, translateY: `${110 * direction}%` }),
+   animate: () => ({ opacity: 1, translateY: 0 }),
+   exit: (direction: number) => ({ opacity: 0, translateY: `${-110 * direction}%` }),
 };
