4840417c95f94c2eb182c803d7ee92fe99ae4145
mvrcelo <https.braganca@gmail.com>
2024-09-21 17:59:25 -0300

    style: disable transition on theme change

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -4,5 +4,9 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 
 export const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
-   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+   return (
+      <NextThemesProvider {...props} disableTransitionOnChange={true}>
+         {children}
+      </NextThemesProvider>
+   );
 };
